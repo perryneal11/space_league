661807bcf21187c8cceb5c97ba88e930ce851032
perryneal11 <perryneal11@gmail.com>
2020-12-28 13:51:35 -0500

    healthbar
    healthbar in UI, collision health loss and sound

---
+++ Space_League/Space League/Assets/Scripts/HealthbarController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthbarController : MonoBehaviour
+{
+  public Image healthBar;
+  public float health;
+  public float startHealth;
+  public AudioSource crashAudio;
+
+  public void onTakeDamage(int damage){
+    health = health - damage;
+    healthBar.fillAmount = health / startHealth;
+    crashAudio.Play();
+  }
+}
---
+++ Space_League/Space League/Assets/Scripts/collisionController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionController : MonoBehaviour
+{
+  public HealthbarController healthBar;
+
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag == "Asteroid"){
+      if(healthBar)
+      {
+        healthBar.onTakeDamage(5);
+        Debug.Log("hit");
+      }
+    }
+
+
+
+  }
+}
